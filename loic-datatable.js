578d9e281724e7de1f40683200febd47785e0e9b
Loic Rameau <loic.rameau@gmail.com>
2015-12-10 00:42:48 +0100

    Adding order

--- loic-datatable.js
+++ loic-datatable.js
@@ -4,78 +4,82 @@ Polymer({
         dataTable: {
             type: Object,
             value: {
-                    Header: [
-                        {
-                            Value: "ID",
-                            DisplayMember: "id",
-                            filter: ""
-                        }, {
-                            Value: "Civilité",
-                            DisplayMember: "civ",
-                            filter: ""
-                        }, {
-                            Value: "Nom",
-                            DisplayMember: "name",
-                            filter: ""
-                        }, {
-                            Value: "Prénom",
-                            DisplayMember: "firstname",
-                            filter: ""
-                        }, {
-                            Value: "Formation",
-                            DisplayMember: "formation",
-                            filter: ""
-                        }
-                    ],
-                    Items: [
-                        {
-                            id: 1,
-                            name: "Foo",
-                            firstname: "Bar",
-                            formation: "C#",
-                            civ: "M."
-                        }, {
-                            id: 2,
-                            name: "Foo",
-                            firstname: "Bar",
-                            formation: "C++",
-                            civ: "M."
-                        }, {
-                            id: 3,
-                            name: "Foo",
-                            firstname: "Bar",
-                            formation: "PHP",
-                            civ: "M."
-                        }, {
-                            id: 4,
-                            name: "Bar",
-                            firstname: "Foo",
-                            formation: "PHP",
-                            civ: "Mme"
-                        }, {
-                            id: 5,
-                            name: "Bar",
-                            firstname: "Foo",
-                            formation: "HTML",
-                            civ: "Mme"
-                        }, {
-                            id: 6,
-                            name: "Bar",
-                            firstname: "Foo",
-                            formation: "SQL",
-                            civ: "Mme"
-                        }, {
-                            id: 7,
-                            name: "Toto",
-                            firstname: "Tata",
-                            formation: "SQL",
-                            civ: "M."
-                        }
-                    ]
+                Order: {
+                    Member: "",
+                    Sens: 1 //1 = ASC ; -1 = DESC
+                },
+                Header: [
+                    {
+                        Value: "ID",
+                        DisplayMember: "id",
+                        filter: ""
+                    }, {
+                        Value: "Civilité",
+                        DisplayMember: "civ",
+                        filter: ""
+                    }, {
+                        Value: "Nom",
+                        DisplayMember: "name",
+                        filter: ""
+                    }, {
+                        Value: "Prénom",
+                        DisplayMember: "firstname",
+                        filter: ""
+                    }, {
+                        Value: "Formation",
+                        DisplayMember: "formation",
+                        filter: ""
+                    }
+                ],
+                Items: [
+                    {
+                        id: 1,
+                        name: "Foo",
+                        firstname: "Bar",
+                        formation: "C#",
+                        civ: "M."
+                    }, {
+                        id: 2,
+                        name: "Foo",
+                        firstname: "Bar",
+                        formation: "C++",
+                        civ: "M."
+                    }, {
+                        id: 3,
+                        name: "Foo",
+                        firstname: "Bar",
+                        formation: "PHP",
+                        civ: "M."
+                    }, {
+                        id: 4,
+                        name: "Bar",
+                        firstname: "Foo",
+                        formation: "PHP",
+                        civ: "Mme"
+                    }, {
+                        id: 5,
+                        name: "Bar",
+                        firstname: "Foo",
+                        formation: "HTML",
+                        civ: "Mme"
+                    }, {
+                        id: 6,
+                        name: "Bar",
+                        firstname: "Foo",
+                        formation: "SQL",
+                        civ: "Mme"
+                    }, {
+                        id: 7,
+                        name: "Toto",
+                        firstname: "Tata",
+                        formation: "SQL",
+                        civ: "M."
+                    }
+                ]
             },
-            notify:true
+            notify: true
         },
-        itemFiltered:{
+        itemFiltered: {
             type: Object,
             readOnly: true
         },
@@ -84,11 +88,11 @@ Polymer({
             notify: true,
             value: true
         },
-        currentPage:{
+        currentPage: {
             type: Number,
             value: 0
         },
-        displayByPage:{
+        displayByPage: {
             type: Number,
             value: 5
         },
@@ -96,6 +100,10 @@ Polymer({
             type: String,
             notify: true,
             value: ""
+        },
+        DisplayedItems: {
+            type: Array,
+            computed: "getItems(itemFiltered, displayByPage, currentPage, dataTable.Order.*)"
         }
         //emptyContent:{
         //    type: Boolean,
@@ -103,17 +111,17 @@ Polymer({
         //    value:false
         //}
     },
-    ready:function() {
+    ready: function () {
         //this._setEmptyContent(Polymer.dom(this).children.length === 0);
         //if(!this.emptyContent){
         //    this.initData();
         //}
         this._setItemFiltered(this.dataTable.Items);
     },
-    initData:function(){
+    initData: function () {
         var headers = [], items = [], table = this.querySelector("table");
         var ths = table.querySelectorAll("thead tr th");
-        for(var i = 0; i < ths.length; i++){
+        for (var i = 0; i < ths.length; i++) {
             var header = ths.item(i);
             headers.push({
                 DisplayMember: header.innerText,
@@ -121,75 +129,102 @@ Polymer({
             });
         }
         ths = table.querySelectorAll("tbody tr");
-        for(var i = 0; i < ths.length; i++){
+        for (var i = 0; i < ths.length; i++) {
             var prop = {};
             var tds = ths.item(i).querySelectorAll("td");
-            for(var j = 0; j < tds.length; j++){
+            for (var j = 0; j < tds.length; j++) {
                 var header = headers[j];
                 prop[header.DisplayMember] = tds.item(j).innerText;
             }
             items.push(prop);
         }
 
-        if(headers.length > 0)
+        if (headers.length > 0)
             this.dataTable.Header = headers;
-        if(items.length > 0)
+        if (items.length > 0)
             this.dataTable.Items = items;
     },
     getIn: function (item, member) {
         return item[member];
     },
-    unique:function(array, member){
+    unique: function (array, member) {
         var a = [];
-        array.forEach(function(item){
-            if(a.indexOf(item[member]) === -1)
+        array.forEach(function (item) {
+            if (a.indexOf(item[member]) === -1)
                 a.push(item[member]);
         });
         return a;
     },
-    changeSelectFilter:function(e, a){
-        e.model.set("header.filter",e.target.value);
+    changeSelectFilter: function (e, a) {
+        e.model.set("header.filter", e.target.value);
         this._filter();
     },
-    getItems:function(array, nb, page){
+    getItems: function (array, nb, page, order) {
         var a = [];
-        array.forEach(function(item, index) {
-            if(
-                    index >= (page*nb)
+        this.order(array, order.base).forEach(function (item, index) {
+            if (
+                index >= (page * nb)
             )
-            if(a.length < nb)
-                a.push(item);
+                if (a.length < nb)
+                    a.push(item);
         });
         return a;
     },
-    pageDown:function(){
-        if(this.currentPage > 0)
+    pageDown: function () {
+        if (this.currentPage > 0)
             this.currentPage--;
     },
-    pageUp:function(){
-        if(this.currentPage < (this.itemFiltered.length/this.displayByPage))
+    pageUp: function () {
+        if (this.currentPage < (this.itemFiltered.length / this.displayByPage))
             this.currentPage++;
     },
-    currentPageDisplay:function(page){return page+1;},
-    getNbPage:function(itemFiltered, displayByPage){
+    currentPageDisplay: function (page) {
+        return page + 1;
+    },
+    getNbPage: function (itemFiltered, displayByPage) {
         return parseInt(itemFiltered.length / displayByPage, 10) + 1;
     },
-    reorder:function(e){
-        this._setItemFiltered(this.dataTable.Items);
+    reorder: function (e) {
+        var member = e.model.get("header.DisplayMember"), inverse = this.dataTable.Order.Member === member;
+        if (inverse) {
+            if (this.dataTable.Order.Sens === 1)
+                this.set("dataTable.Order.Sens", -1);
+            else if (this.dataTable.Order.Sens === -1)
+                this.set("dataTable.Order.Sens", 1);
+        }
+        this.set("dataTable.Order.Member",member);
     },
-    _filter:function(){
+    _filter: function () {
         var header = this.dataTable.Header;
-        this._setItemFiltered(this.dataTable.Items.filter(function(item){
+        this._setItemFiltered(this.dataTable.Items.filter(function (item) {
             var ret = true;
-            header.forEach(function(a){
-                if(a.filter){
+            header.forEach(function (a) {
+                if (a.filter) {
                     ret = ret && item[a.DisplayMember] == a.filter;
                 }
             });
             return ret;
         }));
     },
-    isFiltered:function(header, filter){
-        return header.filter == filter ? "selected": "";
+    isFiltered: function (header, filter) {
+        return header.filter == filter ? "selected" : "";
+    },
+    order: function (array, order) {
+        var a = array.sort(function (current, next) {
+            if (order.Sens === 1) {
+                if (current[order.Member] < next[order.Member])
+                    return -1;
+                if (current[order.Member] > next[order.Member])
+                    return 1;
+                return 0;
+            } else if (order.Sens === -1) {
+                if (current[order.Member] > next[order.Member])
+                    return -1;
+                if (current[order.Member] < next[order.Member])
+                    return 1;
+                return 0;
+            }
+        });
+        return a;
     }
 });
