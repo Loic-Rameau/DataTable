aefe5f0643a5cc503516eecb3890050a9f4374c1
Loic Rameau <loic.rameau@gmail.com>
2015-12-09 21:08:56 +0100

    Get table and update datatable
    Get table and update datatable if a table is given as content

--- loic-datatable.js
+++ loic-datatable.js
@@ -1,31 +1,138 @@
 Polymer({
-  is: 'loic-datatable',
-  properties: {
-    dataTable: {
-      type: Object,
-      value: function() {
-        return {
-          Header: []
-        };
-      }
+    is: 'loic-datatable',
+    properties: {
+        dataTable: {
+            type: Object,
+            value: {
+                    Header: [
+                        {
+                            Value: "ID",
+                            DisplayMember: "id"
+                        }, {
+                            Value: "Civilité",
+                            DisplayMember: "civ"
+                        }, {
+                            Value: "Nom",
+                            DisplayMember: "name"
+                        }, {
+                            Value: "Prénom",
+                            DisplayMember: "firstname"
+                        }, {
+                            Value: "Formation",
+                            DisplayMember: "formation"
+                        }
+                    ],
+                    Items: [
+                        {
+                            id: 1,
+                            name: "Foo",
+                            firstname: "Bar",
+                            formation: "C#",
+                            civ: "M."
+                        }, {
+                            id: 1,
+                            name: "Foo",
+                            firstname: "Bar",
+                            formation: "C++",
+                            civ: "M."
+                        }, {
+                            id: 1,
+                            name: "Foo",
+                            firstname: "Bar",
+                            formation: "PHP",
+                            civ: "M."
+                        }, {
+                            id: 1,
+                            name: "Bar",
+                            firstname: "Foo",
+                            formation: "PHP",
+                            civ: "Mme"
+                        }, {
+                            id: 1,
+                            name: "Bar",
+                            firstname: "Foo",
+                            formation: "HTML",
+                            civ: "Mme"
+                        }, {
+                            id: 1,
+                            name: "Bar",
+                            firstname: "Foo",
+                            formation: "SQL",
+                            civ: "Mme"
+                        }, {
+                            id: 1,
+                            name: "Toto",
+                            firstname: "Tata",
+                            formation: "SQL",
+                            civ: "M."
+                        }
+                    ]
+            },
+            notify:true
+        },
+        withBootstrap: {
+            type: Boolean,
+            notify: true,
+            value: true
+        },
+        customClass: {
+            type: String,
+            notify: true,
+            value: ""
+        },
+        emptyContent:{
+            type: Boolean,
+            readOnly: true,
+            value:false
+        }
     },
-    withBootstrap: {
-      type: Boolean,
-      value: true
+
+    ready:function() {
+        this._setEmptyContent(Polymer.dom(this).children.length === 0);
+        if(!this.emptyContent){
+            this.initData();
+        }
+        this.majTableClass(this.withBootstrap, this.customClass);
     },
-    customClass: {
-      type: String,
-      value: ""
+    majTableClass: function (withBootstrap, customClass) {
+        var table = this.querySelector("table");
+        console.log(table);
+        if(table){
+            if(withBootstrap){
+                table.classList.add("table");
+                table.classList.add("table-hover");
+            }
+            if(customClass !== "")
+                table.classList.add(customClass);
+        }
     },
-    classList: {
-      type: String,
-      computed: '_classList(withBootstrap,customClass)'
+    initData:function(){
+        var headers = [], items = [], table = this.querySelector("table");
+        var ths = table.querySelectorAll("thead tr th");
+        for(var i = 0; i < ths.length; i++){
+            var header = ths.item(i);
+            headers.push({
+                DisplayMember: header.innerText,
+                Value: header.innerText
+            });
+        }
+        ths = table.querySelectorAll("tbody tr");
+        for(var i = 0; i < ths.length; i++){
+            var prop = {};
+            var tds = ths.item(i).querySelectorAll("td");
+            for(var j = 0; j < tds.length; j++){
+                var header = headers[j];
+                prop[header.DisplayMember] = tds.item(j).innerText;
+            }
+            items.push(prop);
+        }
+
+        if(headers.length > 0)
+            this.dataTable.Header = headers;
+        if(items.length > 0)
+            this.dataTable.Items = items;
+    },
+    getIn: function (item, member) {
+        return item[member];
     }
-  },
-  _classList: function(withBootstrap, customClass) {
-    if (withBootstrap) {
-      return "table table-hover" + customClass;
-    }
-    return customClass;
-  }
 });
